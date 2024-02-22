bd05ffc71e32467f3127314b213f7ded1a466449
Developerding <gerald.ding.2021@scis.smu.edu.sg>
2024-02-22 23:38:18 +0800

    frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,107 +1,69 @@
 import { useState } from 'react';
 import './App.css';
-import * as grpc from '@grpc/grpc-js';
-import { backendServiceClient } from './proto/backend_grpc_pb';
-import * as backendMessages from './proto/backend_pb';
-
-const validate = (staffPassId: string, client: backendServiceClient) => {
-  const [teamName, setTeamName] = useState('');
-  const [gifts, setGifts] = useState<backendMessages.Gift[]>([]);
-    let getTeam = new backendMessages.GetTeamRequest();
-    getTeam.setStaffPassId(staffPassId);
-    client.getTeam(getTeam, (err, response: backendMessages.GetTeamResponse | undefined) => {
-      if (err) {
-        console.log(err.message);
-      }
-      else {
-        console.log(response);
-        if (response?.getGtresp() !== undefined) {
-          setTeamName(response.getGtresp());
-        }
-      }
-    });
-
-    let getGifts = new backendMessages.GetGiftsRequest();
-    getGifts.setTeamName(teamName);
-    client.getGifts(getGifts, (err, response: backendMessages.GetGiftsResponse | undefined) => {
-        if (err) {
-            let checkHistory = new backendMessages.HistoryRequest();
-            checkHistory.setGetteamid("RUST");
-            client.checkHistory(checkHistory, (err, response: backendMessages.HistoryResponse | undefined) => {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                  if(response?.getGiftList() !== undefined) {
-                    setGifts(response.getGiftList());
-                  }
-                }
-            });
-        }
-        else if (response?.getGiftList.length !== undefined) {
-            let redeemGift = new backendMessages.RedeemRequest();
-            for( let i in response.getGiftList()) {
-                let gift = new backendMessages.Gift();
-                gift.setStaffPassId(response.getGiftList()[i].getStaffPassId());
-                gift.setTeamName(response.getGiftList()[i].getTeamName());
-                gift.setTime(response.getGiftList()[i].getTime());
-                gifts.push(gift);
-            }
-            redeemGift.setGiftList(gifts);
-            client.redeemGift(redeemGift, (err, response: backendMessages.RedeemResponse | undefined) => {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                    if(response?.getRresp() !== undefined) {
-                        console.log(response.getRresp());
-                    }
-                }
-            });
-            
-            let updateCreate = new backendMessages.UpdateCreateRequest();
-            client.updateCreate(updateCreate, (err, response: backendMessages.UpdateCreateResponse | undefined) => {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                  if(response?.getUcresp() !== undefined) {
-                    console.log(response.getUcresp());
-                }
-                }
-            });
-
-            let updateRedeemed = new backendMessages.UpdateRedeemedRequest();
-            client.updateRedeemed(updateRedeemed, (err, response: backendMessages.UpdateRedeemedResponse | undefined) => {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                  if(response?.getUrresp() !== undefined) {
-                    console.log(response.getUrresp());
-                }
-                }
-            });
-        }
-    });
-}
+import axios from 'axios';
 
 function App() {
+  
   const [staffPassId, setStaffPassId] = useState('');
-
-  const client = new backendServiceClient(
-    'localhost:50051',
-    grpc.credentials.createInsecure()
-  );
+  const [gifts, setGifts] = useState([]);
+  const [status, setStatus] = useState(0);
+  const [errMessage, setErrMessage] = useState('');
+  const [time, setTime] = useState('');
+  function validate(staffPassId: string) {
+    axios.get('http://localhost:3000/check/' + staffPassId).then((res) => {
+      if(res.data.message === 'Gifts redeemed successfully') {
+        console.log(res.data.gifts);
+        setTime(Date.now().toString());
+        setStatus(1);
+      } else if(res.data.message === 'No new gifts') {
+        setStatus(2);
+        setGifts(res.data.gifts);
+        setStaffPassId(res.data.gifts.slice(-1)['staff_pass_id']);
+        setTime(res.data.gifts.slice(-1)['staff_pass_id'])
+      } else {
+        setStatus(3);
+        setErrMessage(res.data.message);
+      }
+    });
+  
+  }
   
   return (
     <>
       <h1>Gift Redemption Counter</h1>
       <div className="card">
         <input type="text" value={staffPassId} onChange={(e) => setStaffPassId(e.target.value)} />
-        <button onClick={() => validate(staffPassId, client)}>
-          count is {staffPassId}
+        <button onClick={() => validate(staffPassId)}>
+          Redeem!
         </button>
+        {/* <ol>
+          {gifts.map((gift, index) => (
+            <li key={index}>{gift}</li>
+          ))}
+        </ol> */}
+        {status === 1 && (
+          <div>
+            <p>Gifts was redeemed by: {staffPassId} at {time}</p>
+            <ol>
+              {gifts.map((gift, index) => (
+                <li key={index}>{gift}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+        {status === 2 && (
+          <div>
+            <p>Gift was last redeemed by: {staffPassId} at {time}</p>
+            <ol>
+              {gifts.map((gift, index) => (
+                <li key={index}>{gift}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+        {status === 3 && (
+          <p>Error faced: {errMessage}</p>
+          )}
       </div>
     </>
   )
--- in-memory/client.js
+++ in-memory/client.js
@@ -2,87 +2,109 @@ const grpc = require('@grpc/grpc-js')
 const backendServices = require('./proto/backend_grpc_pb')
 const backendMessages = require('./proto/backend_pb')
 
-function main() {
+
+var express = require("express");
+var app = express();
+const cors = require("cors");
+var allowedOrigins = [
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
+  "http://localhost:8081",
+  "http://127.0.0.1:8081",
+  "*"
+];
+app.use(
+  express.json(),
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+app.get("/check/:staffPassId", (req, res) => {
     const client = new backendServices.backendServiceClient('localhost:50051', grpc.credentials.createInsecure());
     let gifts = []
     // Read function
     let getTeam = new backendMessages.GetTeamRequest();
-    getTeam.setStaffPassId("BOSS_T000000001P");
+    getTeam.setStaffPassId(req.params.staffPassId);
     client.getTeam(getTeam, function(err, response) {
         if (err) {
-            let checkHistory = new backendMessages.HistoryRequest();
-            checkHistory.setGetteamid("RUST");
-            client.checkHistory(checkHistory, function(err, response) {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                    console.log(response.array);
-                }
-            });
+            res.send(err.message);
         }
         else {
-            console.log(typeof response.getGtresp());
-        }
-    });
-
-    let getGifts = new backendMessages.GetGiftsRequest();
-    getGifts.setTeamName("RUST");
-    client.getGifts(getGifts, function(err, response) {
-        if (err) {
-            console.log(err.message);
             let checkHistory = new backendMessages.HistoryRequest();
-            checkHistory.setGetteamid("RUST");
-            client.checkHistory(checkHistory, function(err, response) {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                    console.log(response.getGiftList());
-                }
-            });
-        }
-        else if (response.getGiftList().length > 0) {
-            let redeemGift = new backendMessages.RedeemRequest();
-            for(i in response.getGiftList()) {
-                let gift = new backendMessages.Gift();
-                gift.setStaffPassId(response.getGiftList()[i].getStaffPassId());
-                gift.setTeamName(response.getGiftList()[i]);
-                gift.setTime(response.getGiftList()[i][2]);
-                gifts.push(gift);
-            }
-            redeemGift.setGiftList(gifts);
-            client.redeemGift(redeemGift, function(err, response) {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                    console.log(response.getRresp());
-                }
-            });
+            let responseArray = JSON.parse(response.getGtresp());
+            if(responseArray['table'] == 'redeemed') {
+                checkHistory.setGetteamid(responseArray['team_name']);
+                client.checkHistory(checkHistory, function(err, response) {
+                    if (err) {
+                        res.send(err.message);
+                    }
+                    else {
+                        res.send(response.array);
+                    }
+                });
+            } else {
+                let getGifts = new backendMessages.GetGiftsRequest();
+                getGifts.setTeamName(responseArray['team_name']);
+                client.getGifts(getGifts, function(err, response) {
+                    if (err) {
+                        res.send(err.message);
+                    }
+                    else if (response.getGiftList().length > 0) {
+                        let redeemGift = new backendMessages.RedeemRequest();
+                        for(i in response.getGiftList()) {
+                            let gift = new backendMessages.Gift();
+                            gift.setStaffPassId(response.getGiftList()[i].getStaffPassId());
+                            gift.setTeamName(response.getGiftList()[i].getTeamName());
+                            gift.setTime(response.getGiftList()[i].getTime());
+                            gifts.push(gift);
+                        }
+                        redeemGift.setGiftList(gifts);
+                        client.redeemGift(redeemGift, function(err, response) {
+                            if (err) {
+                                res.send(err.message);
+                            }
+                        });
+                        
+                        let updateCreate = new backendMessages.UpdateCreateRequest();
+                        client.updateCreate(updateCreate, function(err, response) {
+                            if (err) {
+                                res.send(err.message);
+                            }
+                        });
             
-            let updateCreate = new backendMessages.UpdateCreateRequest();
-            client.updateCreate(updateCreate, function(err, response) {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                    console.log(response.getUcresp());
-                }
-            });
-
-            let updateRedeemed = new backendMessages.UpdateRedeemedRequest();
-            client.updateRedeemed(updateRedeemed, function(err, response) {
-                if (err) {
-                    console.log(err.message);
-                }
-                else {
-                    console.log(response.getUrresp());
-                }
-            });
-        } else {
-
+                        let updateRedeemed = new backendMessages.UpdateRedeemedRequest();
+                        client.updateRedeemed(updateRedeemed, function(err, response) {
+                            if (err) {
+                                res.send(err.message);
+                            }
+                            else {
+                                console.log(gifts);
+                                res.send({message: "Gifts redeemed successfully", gifts: JSON.parse(response.array)});
+                            }
+                        });
+                    } else {
+                        res.send("No gifts available for this team");
+                    }
+                });
+            }
         }
     });
-}
-main();
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+module.exports.app = app;
